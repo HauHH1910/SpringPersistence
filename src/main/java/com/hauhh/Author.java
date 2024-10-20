b1e2e34f9f6bdc800928b4e1f678aaaa13180a9f
HaiHau <hhhau1910@gmail.com>
2024-10-20 18:48:57 +0700

    create entity Author for the example

---
+++ src/main/java/com/hauhh/Author.java
@@ -0,0 +1,26 @@
+package com.hauhh;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.TABLE
+    )
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
+    private Long id;
+
+    @Version
+    private int version;
+
+    private String firstName;
+
+    private String lastName;
+}
